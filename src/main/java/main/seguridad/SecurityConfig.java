6c318873ef2a6fc281e2da83c2911f75b6908de9
Carlos Barreiro <barreirocarlos2001@gmail.com>
2022-12-20 14:23:19 +0100

    .

--- src/main/java/main/seguridad/SecurityConfig.java
+++ src/main/java/main/seguridad/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     protected void configure(HttpSecurity http) throws Exception {
            
             http.authorizeRequests()
-                    .antMatchers("/","/alumnos").permitAll()
+                    .antMatchers("/","/usuaios").permitAll()
                    // .antMatchers("/admin").hasRole("ROLE_ADMIN")              
                     .and()
                     .formLogin()
